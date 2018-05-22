b0e1d809b7bf2e1171be4059b18b07b8e6985b78
Jan Votroubek <jan.votroubek@firma.seznam.cz>
2018-05-22 16:29:21 +0200

    Update

--- bot.py
+++ bot.py
@@ -25,8 +25,7 @@ TOKEN = "554022144:AAFDYJkAZSH6flnmPujbJ1nmtnaJ7DPMTZA"
 PORT = int(os.environ.get('PORT', '8443'))
 
 # Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
@@ -75,9 +74,6 @@ def main():
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("pricecvz", pricecvz))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
     dp.add_error_handler(error)
